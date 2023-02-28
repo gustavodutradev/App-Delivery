f6e6d4e77d0f10db7d82cc5a627198fa091ba332
Juliana <julianamq20@gmail.com>
2023-02-28 15:21:56 -0300

    feat : login user

--- back-end/src/service/login.service.js
+++ back-end/src/service/login.service.js
@@ -1,14 +1,30 @@
 import userModel from '../database/models/Users';
+import { checkPassword, encryptPassword } from '../utils/bcrypt';
+import { generateToken } from '../utils/token';
+import NotFound from '../utils/errors/notFound';
+
 
 async function createUser(user) {
-    const {name, email, role, password} = user;
+    const { name, email, role, password } = user;
     const passwordCrypt = encryptPassword(password);
-    const newUser = await userModel.create({...user, password: passwordCrypt});
+    const newUser = await userModel.create({ ...user, password: passwordCrypt });
     const token = generateToken(newUser);
-    return {name, email, role , token}
-  
-} 
+    return { name, email, role, token }
 
-async function loginUser(){
+}
 
+async function loginUser(user) {
+    const { email, password } = user;
+    const userFound = await userModel.findOne({ where: { email } });
+    const { name, role } = userFound;
+    const isValidPassword = checkPassword(password, userFound.password);
+    const token = generateToken(userFound);
+    if (!userFound) throw new NotFound('Email or Password is invalid!');
+    if (!isValidPassword) throw new NotFound('Email or Password is invalid!');
+    return { name, email, role, token }
+}
+
+module.exports = {
+    createUser,
+    loginUser,
 }
