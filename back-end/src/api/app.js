3c6ae738f19eb640adc1e23d389eb7a20bb06a5d
Ricardo_Souza <rl03souza@gmail.com>
2023-03-01 18:06:52 -0300

    Feat: adding cors to api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+const cors = require('cors');
+
 const app = express();
-const errorHandle = require('../middleware/errorHandle')
+const errorHandle = require('../middleware/errorHandle');
 
 app.use(express.json());
+app.use(cors);
 // app.get('/coffee', (_req, res) => res.status(418).end());
-app.use(routes)
+app.use(routes);
 app.use(errorHandle);
 
 module.exports = app;
