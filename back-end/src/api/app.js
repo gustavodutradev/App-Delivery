107012c9cea71dcd1d6cb7025968caac34c2626e
Ricardo_Souza <rl03souza@gmail.com>
2023-03-02 14:46:57 -0300

    Fix: script run dev

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,7 +6,7 @@ const app = express();
 const errorHandle = require('../middleware/errorHandle');
 
 app.use(express.json());
-app.use(cors);
+// app.use(cors);
 // app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(routes);
 app.use(errorHandle);
