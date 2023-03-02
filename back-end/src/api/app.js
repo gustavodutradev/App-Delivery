9b405118ff0bc947c9eacaddb20f57fa49e233f9
Ricardo_Souza <rl03souza@gmail.com>
2023-03-02 14:56:34 -0300

    Chore: add cors to api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 const errorHandle = require('../middleware/errorHandle');
 
 app.use(express.json());
+app.use(cors());
 // app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(routes);
 app.use(errorHandle);
