355a0c646986037264c74637e566b7308aaa1b9c
Ricardo_Souza <rl03souza@gmail.com>
2023-03-03 16:23:08 -0300

    Feat: creating service for products

---
+++ back-end/src/controller/products.controller.js
@@ -0,0 +1,14 @@
+const productsService = require('../service/products.service');
+
+const getProducts = async (_req, res, next)=> {
+  try {
+    const allProducts = await productsService.getProducts();
+    return res.status(200).json(allProducts);
+  } catch (err) {
+    return next(err);
+  };
+};
+
+module.exports = {
+  getProducts,
+};
