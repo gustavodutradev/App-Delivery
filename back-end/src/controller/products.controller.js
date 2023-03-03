18d4a08a8295927257331d645cb9d058ebe68423
Ricardo_Souza <rl03souza@gmail.com>
2023-03-03 16:30:40 -0300

    Fix: lint errors

--- back-end/src/controller/products.controller.js
+++ back-end/src/controller/products.controller.js
@@ -1,12 +1,12 @@
 const productsService = require('../service/products.service');
 
-const getProducts = async (_req, res, next)=> {
+const getProducts = async (_req, res, next) => {
   try {
     const allProducts = await productsService.getProducts();
     return res.status(200).json(allProducts);
   } catch (err) {
     return next(err);
-  };
+  }
 };
 
 module.exports = {
