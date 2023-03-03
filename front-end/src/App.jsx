b48e7ab5628ca298a335d0344bb067e808dcf9e5
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-03 19:09:21 -0300

    feat: adds products page to app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import RouteWithNav from './components/RouteWithNav';
 import LoginPage from './pages/Login/LoginPage';
+import ProductsPage from './pages/Products/ProductsPage';
 import RegisterPage from './pages/Register/RegisterPage';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <LoginPage /> } />
         <Route path="/register" element={ <RegisterPage /> } />
-        <RouteWithNav path="/customer/products" />
+        <RouteWithNav path="/customer/products" element={ <ProductsPage /> } />
       </Routes>
     </div>
   );
---
+++ front-end/src/pages/Products/ProductsPage.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+function ProductsPage() {
+  useEffect(() => {
+
+  });
+  return (
+    <div>ProductsPage</div>
+  );
+}
+
+export default ProductsPage;
