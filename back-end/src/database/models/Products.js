a2c43ad939b07a30d0adb73bed3aab9ad8cce157
Ricardo_Souza <rl03souza@gmail.com>
2023-03-06 16:20:25 -0300

    Fix: add autoincrement at products model

--- back-end/src/database/models/Products.js
+++ back-end/src/database/models/Products.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product', {
     id: {
       type: DataTypes.INTEGER,
-      primaryKey: true
+      primaryKey: true,
+      autoIncrement: true,
     },
     name: {
       type: DataTypes.STRING,
