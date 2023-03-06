81128649c4825354b1ee9e6c9f92a2498eed8c0c
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-06 18:37:29 -0300

    feat: add NavBar styles

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -2,6 +2,72 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+
+const SNav = styled.nav`
+  ${tw`
+  w-full
+  flex
+  justify-between
+  p-1.5
+  `}
+  span {
+    display: flex;
+    text-align: center;
+    align-items: center;
+  }
+  background-color: #333333;
+`
+const SRigth = styled.ul`
+  ${tw`
+  flex
+  `}
+    li {
+    float: right;
+    list-style-type: none;
+  }
+  span {
+    color: #FC0;
+    font-weight: bold;
+  }
+  button {
+    background-color: #333333;
+    border: 1px solid #FC0;
+    color: #FC0;
+    border-radius: 10px;
+    font-weight: lighter;
+      &:hover {
+        background-color:#FC0;
+        border: 1px solid #333333;
+        color: #333333;
+    }
+  }
+
+`
+
+const SLeft = styled.ul`
+  ${tw`
+  flex
+  `}
+    li {
+    float: left;
+    list-style-type: none;
+  }
+  button {
+    background-color: #333333;
+    border: 1px solid #FC0;
+    color: #FC0;
+    border-radius: 10px;
+    font-weight: lighter;
+      &:hover {
+      background-color:#FC0;
+      border: 1px solid #333333;
+      color: #333333;
+    }
+  }
+`
+
 
 function NavBar() {
   const [username, setUsername] = useState('');
@@ -16,32 +82,34 @@ function NavBar() {
   }, [setUsername, name]);
 
   return (
-    <nav>
+    <SNav>
+      <SLeft>
+        <Button
+          name="Produtos"
+          datatestId="customer_products__element-navbar-link-products"
+          onClick={ goToProducts }
+        />
 
-      <Button
-        name="Produtos"
-        datatestId="customer_products__element-navbar-link-products"
-        onClick={ goToProducts }
-      />
+        <Button
+          name="Pedidos"
+          datatestId="customer_products__element-navbar-link-orders"
+          onClick={ goToOrders }
+        />
+      </SLeft>
+      <SRigth>
+        <span
+          datatest-id="customer_products__element-navbar-user-full-name"
+        >
+          { `Olá, ${username} ` }
+        </span>
 
-      <Button
-        name="Pedidos"
-        datatestId="customer_products__element-navbar-link-orders"
-        onClick={ goToOrders }
-      />
-
-      <span
-        datatest-id="customer_products__element-navbar-user-full-name"
-      >
-        { username }
-      </span>
-
-      <Button
-        name="Sair"
-        datatestId="customer_products__element-navbar-link-logout"
-        // onClick={}
-      />
-    </nav>
+        <Button
+          name="Sair"
+          datatestId="customer_products__element-navbar-link-logout"
+          // onClick={}
+        />
+      </SRigth>
+    </SNav>
   );
 }
 
