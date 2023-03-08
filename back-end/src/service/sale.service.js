86e43de80028f912be0e7b6e9dc0e4a88e598473
Ricardo_Souza <rl03souza@gmail.com>
2023-03-08 16:20:19 -0300

    Feat: add createSale method

--- back-end/src/service/sale.service.js
+++ back-end/src/service/sale.service.js
@@ -1,10 +1,52 @@
-const { User } = require('../database/models');
+const { User, Sale, SalesProduct, sequelize } = require('../database/models');
 const { verifyToken } = require('../utils/token');
 
 const getAllSellers = async (token) => {
-    verifyToken(token);
-    const sellers = await User.findAll({ where: { role: 'seller' } });
-    return sellers;
+  verifyToken(token);
+  const sellers = await User.findAll({ where: { role: 'seller' } });
+  return sellers;
 };
 
-module.exports = { getAllSellers };
+const createSaleObj = (sale) => {
+  const { userId, sellerId, address, totalPrice } = sale;
+  const now = new Date();
+  const utcDate = now.toUTCString();
+
+  const newSale = {
+    userId,
+    sellerId,
+    totalPrice,
+    deliveryAddress: address.street,
+    deliveryNumber: address.number,
+    saleDate: utcDate,
+    status: sale.status || 'Pendente',
+  };
+
+  return newSale;
+};
+
+const createSale = async (sale, token) => {
+  verifyToken(token);
+  const t = await sequelize.transaction();
+  const newSale = createSaleObj(sale);
+  const { products } = sale;
+
+  try {
+    const { id: saleId } = await Sale.create(newSale, { transaction: t });
+
+    const createdSaleProduct = products.map(({ id, quantity }) => SalesProduct
+      .create({ saleId, productId: id, quantity }, { transaction: t }));
+
+    await Promise.all(createdSaleProduct);
+    await t.commit();
+
+    return saleId;
+  } catch (err) {
+    await t.rollback();
+  }
+};
+
+module.exports = {
+  getAllSellers,
+  createSale,
+};
