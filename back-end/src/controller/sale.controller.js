c364388ca319cc0e4a6ac319a29081d1f6f166c5
Ricardo_Souza <rl03souza@gmail.com>
2023-03-08 16:20:42 -0300

    Feat: createSale method on controller

--- back-end/src/controller/sale.controller.js
+++ back-end/src/controller/sale.controller.js
@@ -1,13 +1,37 @@
-const { getAllSellers } = require('../service/sale.service');
+const saleService = require('../service/sale.service');
 
 const getAllSeller = async (req, res, next) => {
     const token = req.headers.authorization;
     try {
-        const allUser = await getAllSellers(token);
-        console.log(allUser);
+        const allUser = await saleService.getAllSellers(token);
         return res.status(200).json(allUser);
     } catch (error) {
         return next(error);
     }
 };
-module.exports = { getAllSeller };
+
+// obj do body = {
+//     userId,
+//     sellerId,
+//     products: lista de produtos,
+//     address: {
+//         street,
+//         number,
+//     }, 
+//     totalPrice,
+// };
+
+const createSale = async (req, res, next) => {
+    const token = req.headers.authorization;
+    const sale = req.body;
+    try {
+        const saleId = await saleService.createSale(sale, token);
+        return res.status(201).json({ saleId });
+    } catch (err) {
+        return next(err);
+    }
+};
+module.exports = {
+    getAllSeller,
+    createSale,
+};
