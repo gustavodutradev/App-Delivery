21860ab5a4a703520bf7030ce50d7e53cfb35a80
Ricardo_Souza <rl03souza@gmail.com>
2023-03-08 16:21:01 -0300

    Feat: create route to create a new sale

--- back-end/src/api/routes/sales.routes.js
+++ back-end/src/api/routes/sales.routes.js
@@ -4,5 +4,6 @@ const salesController = require('../../controller/sale.controller');
 const saleRoutes = express.Router();
 
 saleRoutes.get('/sellers', (req, res, next) => salesController.getAllSeller(req, res, next));
+saleRoutes.post('/', (req, res, next) => salesController.createSale(req, res, next));
 
 module.exports = saleRoutes;
