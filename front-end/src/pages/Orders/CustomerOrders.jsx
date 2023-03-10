d8617bf441bbfac544cba0636e0eb3915afc195a
gustavoBarros <gustavo.dutra11@gmail.com>
2023-03-10 17:46:39 -0300

    refactor: use component OrderCard

--- front-end/src/pages/Orders/CustomerOrders.jsx
+++ front-end/src/pages/Orders/CustomerOrders.jsx
@@ -1,39 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axiosRequest from '../../utils/axios';
+import SOrdersTable from './styles/SOrdersTable';
+import OrderCard from './OrderCard';
 
 function CustomerOrders() {
   const userId = useSelector((state) => state.user.id);
   const axios = axiosRequest();
-
   const [orders, setOrders] = useState([]);
 
-  // const getUserOrders = async () => {
-  //   const orderss = await axios.get(`/sales/user/${userId}`);
-  //   console.log(orderss);
-  // };
-
   useEffect(() => {
-    console.log(userId);
     axios.get(`/sales/user/${userId}`)
       .then((response) => {
         setOrders(response.data);
       });
   }, []);
 
-  console.log(orders);
-
   return (
     <div>
-      <h1>Orders</h1>
-      {orders.map((order) => (
-        <div key={ order.id }>
-          <p>{order.id}</p>
-          <p>{order.status}</p>
-          <p>{order.saleDate}</p>
-          <p>{order.totalPrice}</p>
-        </div>
-      ))}
+      <SOrdersTable>
+        {orders.map((order, index) => (
+          <OrderCard
+            key={ index }
+            order={ order }
+          />
+        ))}
+      </SOrdersTable>
     </div>
   );
 }
