0f1bc4dfc140a0f0c00763898f458576e09ec8b9
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-10 14:54:03 -0300

    feat: creates orderList

---
+++ front-end/src/pages/CustomerOrderDetails/OrderList.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import TotalPrice from '../../components/TotalPrice';
+import ProductItem from './ProductItem';
+import { OrderContext } from './OrderDetails';
+
+const ListContainer = styled.div`
+  ${tw`
+    relative
+  `}
+  padding-bottom: 10rem; // deve ser relativo ao height do totalPrice
+`;
+
+function OrderList() {
+  const products = useContext(OrderContext);
+
+  return (
+    <ListContainer>
+      <ul>
+        {products
+          .filter((e) => e.quantity > 0)
+          .map((e, i) => (
+            <li key={ i }>
+              <ProductItem product={ e } index={ i } />
+            </li>
+          ))}
+      </ul>
+      <TotalPrice
+        products={ products }
+        dataTestid="customer_order_details__element-order-total-price"
+      />
+    </ListContainer>
+  );
+}
+
+OrderList.propTypes = {};
+
+export default OrderList;
