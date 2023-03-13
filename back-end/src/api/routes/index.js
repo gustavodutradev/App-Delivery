05329db2f2d5b5b99fc7e5742f88ed9aced07742
Ricardo_Souza <rl03souza@gmail.com>
2023-03-13 15:37:39 -0300

    Feat: create router to /admin

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -3,6 +3,7 @@ const loginRoutes = require('./login.routes');
 const registerRoutes = require('./register.routes');
 const productRoutes = require('./products.routes');
 const saleRoutes = require('./sales.routes');
+const adminRoutes = require('./admin.routes');
 
 const routes = express.Router();
 
@@ -10,5 +11,6 @@ routes.use('/login', loginRoutes);
 routes.use('/register', registerRoutes);
 routes.use('/products', productRoutes);
 routes.use('/sales', saleRoutes);
+routes.use('/admin', adminRoutes);
 
 module.exports = routes;
