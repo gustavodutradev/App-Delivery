988559988b974511dc0c6fdcc75e46b80b4c9958
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-13 11:13:24 -0300

    Revert "lint: test rename file"
    This reverts commit 77a125ed5c607f13f47ab1053b9616ba47e8ea3c.

--- front-end/src/pages/CustomerOrderDetails/OrderDetails.jsx
+++ front-end/src/pages/CustomerOrderDetails/OrderDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import axiosRequest from '../../utils/axiosRequest';
+import axiosRequest from '../../utils/axios';
 import ListHeader from './ListHeader';
 import OrderContext from './OrderContext';
 import OrderList from './OrderList';
