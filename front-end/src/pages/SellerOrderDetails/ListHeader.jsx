3da9d5dcfc7223c384174257a1673042cd1e0e2c
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-13 18:08:38 -0300

    feat: adds the seller version of order details

---
+++ front-end/src/pages/SellerOrderDetails/ListHeader.jsx
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react';
+import { useSelector } from 'react-redux';
+import OrderContext from './OrderContext';
+import Button from '../../components/Button';
+import axiosRequest from '../../utils/axios';
+
+const lintLength = 4;
+const lintLength2 = 10;
+
+function ListHeader() {
+  const order = useContext(OrderContext);
+  const token = useSelector((state) => state.user.token);
+
+  return (
+    <div>
+      <span
+        className="order-index"
+        data-testid="seller_order_details__element-order-details-label-order-id"
+      >
+        {`PEDIDO ${String(order.saleId).padStart(lintLength, '0')}`}
+      </span>
+      <span
+        className="date"
+        data-testid="seller_order_details__element-order-details-label-order-date"
+      >
+        {order.saleDate.slice(0, lintLength2).split('-').reverse().join('/')}
+
+      </span>
+      <span
+        className="status"
+        data-testid="seller_order_details__element-order-details-label-delivery-status"
+      >
+        {order.status}
+
+      </span>
+      <Button
+        className="confirm-delivery"
+        datatestId="seller_order_details__button-preparing-check"
+        type="button"
+        onClick={ async () => {
+          const { status, data } = await axiosRequest({ authorization: token })
+            .put(`/sales/seller/${order.id}`, { status: 'preparando' });
+          if (status) {
+            order.status = data.status;
+          }
+        } }
+        name="PREPARAR PEDIDO"
+        disabled={ order.status.toLowerCase() !== 'pendente' }
+      />
+      <Button
+        className="confirm-delivery"
+        datatestId="seller_order_details__button-dispatch-check"
+        type="button"
+        onClick={ async () => {
+          const { status, data } = await axiosRequest({ authorization: token })
+            .put(`/sales/seller/${order.id}`, { status: 'em trânsito' });
+          if (status) {
+            order.status = data.status;
+          }
+        } }
+        name="SAIU PARA ENTREGA"
+        disabled={ order.status.toLowerCase() !== 'preparando' }
+      />
+    </div>
+  );
+}
+
+export default ListHeader;
