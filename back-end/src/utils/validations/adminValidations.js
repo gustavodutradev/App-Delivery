5ec1b4fa1601f807d191b34be852b75ab28c9833
Ricardo_Souza <rl03souza@gmail.com>
2023-03-13 16:07:37 -0300

    Feat: create admin validations

---
+++ back-end/src/utils/validations/adminValidations.js
@@ -0,0 +1,27 @@
+const { registerValidations } = require('./loginValidations');
+
+const verifyAdminRole = (role) => {
+  if (role !== 'administrator') throw new InvalidParams('You must be an admin to utilize this route.');
+};
+
+const validateRole = (role) => {
+  if (!role) throw new InvalidParams('Field "role" is required.');
+  const checkRoles = role === 'customer' || role === 'seller' || role === 'administrator';
+  if (!checkRoles) throw new InvalidParams('Invalid role.');
+};
+
+const checkUserExistence = (userFound) => {
+  if (userFound) throw new ConflictError('Email or Name already registered.');
+};
+
+const adminRegisterValidations = ({ email, password, name, role }) => {
+  registerValidations({ email, password, name });
+  validateRole(role);
+};
+
+module.exports = {
+  verifyAdminRole,
+  validateRole,
+  checkUserExistence,
+  adminRegisterValidations,
+};
