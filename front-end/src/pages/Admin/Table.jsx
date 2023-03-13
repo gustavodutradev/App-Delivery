9db22f774043c713de06a4084d410d797c7b2dc0
Ricardo_Souza <rl03souza@gmail.com>
2023-03-13 20:15:40 -0300

    Feat: create table to list all users

---
+++ front-end/src/pages/Admin/Table.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import TableBody from './TableBody'
+import TableHeader from './TableHeader'
+
+export default function Table() {
+  return (
+    <fieldset>
+      <table 
+        align="center"
+        bgcolor="silver"
+        border="5"
+        cellPadding="10"
+        cellSpacing="5"
+      >
+        <TableHeader />
+        <TableBody />
+      </table>
+    </fieldset>
+  )
+}
---
+++ front-end/src/pages/Admin/TableBody.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import Button from '../../components/Button';
+import axiosRequest from '../../utils/axios';
+
+export default function TableBody() {
+  const [users, setUsers] = useState([]);
+  const token = useSelector((state) => state.user.token);
+  
+  const handleRequest = async () => {
+    const { data } = await axiosRequest({ authorization: token }).get('/admin');
+    setUsers(data);
+  }
+
+  const deleteUser = async (email) => {
+    await axiosRequest({ authorization: token })
+      .delete('/admin', { data: { email } });
+  }
+  
+  useEffect(() => {
+    handleRequest();
+  }, [users]);
+
+  return (
+    <tbody align='center'>
+        { users.map(({ name, email, role }, index) => (
+        <tr key={index}>
+          <td data-testid={`admin_manage__element-user-table-item-number-${index}`}>{ index }</td>
+          <td data-testid={`admin_manage__element-user-table-name-${index}`}>{ name }</td>
+          <td data-testid={`admin_manage__element-user-table-email-${index}`}>{ email }</td>
+          <td data-testid={`admin_manage__element-user-table-role-${index}`}>{ role }</td>
+          <td>
+            <Button
+              name="Excluir"
+              data-testid={`admin_manage__element-user-table-remove-${index}`}
+              type={"button"}
+              onClick={ () => deleteUser(email) }
+              disabled={false}
+            />
+          </td>
+        </tr>
+        ))}
+    </tbody>
+  )
+}
