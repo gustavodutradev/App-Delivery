6ab4430847b754a9c40e07e57e5c8b8eed47a91f
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-22 16:02:13 -0300

    refactor: change login header styles

--- front-end/src/pages/Login/styles/SFormHeader.jsx
+++ front-end/src/pages/Login/styles/SFormHeader.jsx
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 const SFormHeader = styled.div`
   // SMARTPHONE
   @media only screen and (min-width: 360px) and (max-width: 480px) {
-    position: fixed;
-    top: 4.5rem;
-    color: #333;
-    font-size: 23px;
-    font-weight: bold;
-    text-shadow: 0.3rem 0.3rem 0.3rem #000000b5;
+    position: absolute;
+    width: 100vw;
+    min-height: 250px;
+    filter: drop-shadow(0px 15px 27px rgba(0, 0, 0, 0.25));
+    left: 0px;
+    top: 0px;
+    border-radius: 0px 0px 30px 30px;
+    background: #433f5a;
+    background: -webkit-linear-gradient(145deg, #433f5a 0%, #ff6442 50%, #433f5a 100%);
+    background: linear-gradient(145deg, #433f5a 0%, #ff6442 50%, #433f5a 100%);
 }
 `;
 
