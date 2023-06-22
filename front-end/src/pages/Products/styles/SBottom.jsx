143192273c39fc6e7b7c59526ba2b1e9c54d12ce
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-22 16:03:08 -0300

    refactor: add screenmedia and button styles

--- front-end/src/pages/Products/styles/SBottom.jsx
+++ front-end/src/pages/Products/styles/SBottom.jsx
@@ -2,12 +2,21 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const BottomContainer = styled.div`
+@media only screen and (min-width: 360px) and (max-width: 480px) {
   ${tw`
     w-full
     flex
     flex-col
     justify-between
   `}
+
+button {
+  border-radius: 8px;
+  color: #433f5a;
+  background-color: #fff;
+  border: 1px solid rgb(255,197,0);
+}
+
   .product-title {
     ${tw`
         text-center
@@ -16,6 +25,7 @@ const BottomContainer = styled.div`
     `}
     color: white;
   }
+}
 `;
 
 export default BottomContainer;
