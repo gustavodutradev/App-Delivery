805f9f1580a4e9042d74642cd7cd2bf298eade8b
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-27 17:06:26 -0300

    fix: db name

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -4,7 +4,7 @@ const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app-dev'}`,
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
