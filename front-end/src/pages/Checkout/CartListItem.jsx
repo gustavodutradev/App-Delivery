848e08929dca28f161e3de89c8f2762f722b39f8
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-27 17:08:18 -0300

    refactor: add buttons change quantity on checkout

--- front-end/src/pages/Checkout/CartListItem.jsx
+++ front-end/src/pages/Checkout/CartListItem.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import Button from '../../components/Button';
 import { changeQuantity } from '../../redux/slices/cartSlice';
 
 // styles
-import SListItem from './styles/SListItem';
+import QuantityContainer from '../Products/styles/SQuantity';
 import LeftContainer from './styles/SLeftContainer';
+import SListItem from './styles/SListItem';
 import RightContainer from './styles/SRightContainer';
 
 function CartListItem(props) {
@@ -20,52 +22,62 @@ function CartListItem(props) {
     return String(number).replace('.', ',');
   };
 
+  const handleNegative = (number) => (number < 0 ? 0 : number);
+
+  const setQuantity = (number) => {
+    dispatch(changeQuantity({
+      quantity: number,
+      id: product.id,
+    }));
+  };
+
   return (
     <SListItem>
       <LeftContainer>
-        <span
-          className="index"
-          data-testid={ `customer_checkout__element-order-table-item-number-${index}` }
-        >
-          {index + 1}
-        </span>
+        <img
+          src={ product.urlImage }
+          alt=""
+          data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+        />
+      </LeftContainer>
+      <RightContainer>
         <span
           className="description"
           data-testid={ `customer_checkout__element-order-table-name-${index}` }
         >
           {product.name}
         </span>
-      </LeftContainer>
-      <RightContainer>
-        <span
-          className="quantity"
-          data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
-        >
-          {product.quantity}
-        </span>
-        <span
-          className="unitPrice"
-          data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
-        >
-          {`R$: ${formatValue(product.price)}`}
-        </span>
-        <span
-          className="subTotal"
-          data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
-        >
-          {`R$: ${formatValue(product.price * product.quantity)}`}
-        </span>
-        <Button
-          name="Remover"
-          type="button"
-          onClick={ () => {
-            dispatch(changeQuantity({
-              id: product.id,
-              quantity: 0,
-            }));
-          } }
-          datatestId={ `customer_checkout__element-order-table-remove-${index}` }
-        />
+        <QuantityContainer>
+          <Button
+            onClick={ () => { setQuantity(handleNegative(product.quantity - 1)); } }
+            name="-"
+            datatestId={ `customer_products__button-card-rm-item-${product.id}` }
+          />
+          <Button
+            onClick={ () => { setQuantity(handleNegative(product.quantity + 1)); } }
+            name="+"
+            datatestId={ `customer_products__button-card-add-item-${product.id}` }
+          />
+        </QuantityContainer>
+        <div className="product-infos">
+          <span
+            className="subTotal"
+            data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
+          >
+            {`R$ ${formatValue(product.price * product.quantity)}`}
+          </span>
+          <Button
+            name={ <FaTrashAlt /> }
+            type="button"
+            onClick={ () => {
+              dispatch(changeQuantity({
+                id: product.id,
+                quantity: 0,
+              }));
+            } }
+            datatestId={ `customer_checkout__element-order-table-remove-${index}` }
+          />
+        </div>
       </RightContainer>
     </SListItem>
   );
