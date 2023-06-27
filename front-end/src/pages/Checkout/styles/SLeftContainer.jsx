fe0f566f759a50f9c8440712fdcf3addb64ecc7b
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-27 17:09:15 -0300

    refactor: checkout left container css

--- front-end/src/pages/Checkout/styles/SLeftContainer.jsx
+++ front-end/src/pages/Checkout/styles/SLeftContainer.jsx
@@ -1,28 +1,14 @@
 import styled from 'styled-components';
-import tw from 'twin.macro';
 
 const LeftContainer = styled.div`
   @media only screen and (min-width: 360px) and (max-width: 480px) {
-    ${tw`
-      flex
-      justify-center
-      items-center
-      gap-1
-    `}
-    width: 8rem;
-    font-weight: bold;
-    .index {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-width: 1.5rem;
-    background-color: #1f6caf;
+  img {
+    width: 60px;
+    height: fit-content;
     border-radius: 100px;
-    color: white;
-    font-weight: bold;
-    height: 1.5rem;
-    }
-
+    filter: saturate(115%)
+  }
+  
   }
 `;
 
