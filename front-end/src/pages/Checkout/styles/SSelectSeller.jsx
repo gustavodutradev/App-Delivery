280a80ee362554d59fc7eb627c392570d458f64a
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-27 17:10:03 -0300

    refactor: checkout select seller

--- front-end/src/pages/Checkout/styles/SSelectSeller.jsx
+++ front-end/src/pages/Checkout/styles/SSelectSeller.jsx
@@ -3,16 +3,17 @@ import Select from '../../../components/Select';
 
 const SelectSeller = styled(Select)`
   @media only screen and (min-width: 360px) and (max-width: 480px) {
-      select {
-      width: 100px;
-      height: 30px;
-      border-radius: 5px;
-      border: 1px solid #036B52;
-      background-color: #fff;
-      color: #036B52;
-      font-size: 14px;
-      font-weight: bold;
+  select {
+    width: 100%;
+    height: 25px;
+    border-radius: 5px;
+    border: 1px solid #FF6442;
+    background-color: #fff;
+    color: black;
+    font-size: 14px;
+    text-align: center;
   }
+
 }
 `;
 
