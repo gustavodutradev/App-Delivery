9a832350fab4e5b2adf0658d5a37348a4cbb2daa
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-27 17:09:31 -0300

    refactor: list item checkout css

--- front-end/src/pages/Checkout/styles/SListItem.jsx
+++ front-end/src/pages/Checkout/styles/SListItem.jsx
@@ -1,23 +1,24 @@
 import styled from 'styled-components';
+import tw from 'twin.macro';
 
 const SListItem = styled.div`
   @media only screen and (min-width: 360px) and (max-width: 480px) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-    padding: 1rem;
-    margin: 1rem 0;
-    width: 380px;
+    ${tw`
+    flex
+    justify-around
+    items-center
+    `}
+    width: 280px;
+    height: 120px;
     box-shadow: 0.3rem 0.3rem 0.7rem #333333;
-    border-radius: 10px;
-    background-color: rgba(204, 156, 0, 0.486);
+    border-radius: 15px;
+    background-color: #FDFDFD;
+    margin: 1rem 0;
+    position: relative;
 }
 
   span {
-    font-weight: bold;
     font-size: 12px;
-    margin-left: 0.7rem;
   }
 `;
 
