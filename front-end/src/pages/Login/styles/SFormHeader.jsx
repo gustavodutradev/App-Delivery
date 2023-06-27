2cd3c84b7100ff9005652f4148c41c344806091e
gustavoBarros <gustavo.dutra11@gmail.com>
2023-06-27 17:12:50 -0300

    refactor: login css form header

--- front-end/src/pages/Login/styles/SFormHeader.jsx
+++ front-end/src/pages/Login/styles/SFormHeader.jsx
@@ -4,15 +4,19 @@ const SFormHeader = styled.div`
   // SMARTPHONE
   @media only screen and (min-width: 360px) and (max-width: 480px) {
     position: absolute;
-    width: 100vw;
-    min-height: 250px;
+    width: 100%;
+    min-height: 300px;
     filter: drop-shadow(0px 15px 27px rgba(0, 0, 0, 0.25));
-    left: 0px;
-    top: 0px;
+    top: 1px;
+    right: 0px;
     border-radius: 0px 0px 30px 30px;
-    background: #433f5a;
-    background: -webkit-linear-gradient(145deg, #433f5a 0%, #ff6442 50%, #433f5a 100%);
-    background: linear-gradient(145deg, #433f5a 0%, #ff6442 50%, #433f5a 100%);
+    margin: auto;
+
+    img {
+      width: 96vw;
+      object-fit: cover;
+      border-radius: 0px 0px 30px 30px;
+    }
 }
 `;
 
